cf9bc2ad2061c184bc75ec41dedc31a465374c3f
ateeshkumar <ateesh2002@gmail.com>
2023-04-26 00:11:28 +0530

    Master sdfg

---
+++ Fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int fibo(int n){
+    if(n==1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    return fibo(n-1)+fibo(n-2);
+}
+int main(){
+    int n;
+    cout<<"Enter Your No: ";
+    cin>>n;
+    cout<<fibo(n)<<endl;
+
+return 0;
+}
